0a3f2d6af6febd0b7309310358e82f96d5f45d00
Cristian Chira <cristianchira@gmail.com.com>
2019-12-16 13:29:37 +0200

    Update Point class

--- src/classes/Point.java
+++ src/classes/Point.java
@@ -6,12 +6,16 @@ public class Point {
 	public static int y;
 	public int useCount;
 	int[] metrics;
-//    public Point(int x, int y) { this.x = x; this.y = y; }
+   //  public Point(int x, int y) { this.x = x; this.y = y; }
    // public static final Point origin = new Point(0, 0);
+	public static void main(String[] args) {
+		
+		
+	}
     
     static {
     	 
-		System.out.println(" Static initialliser" + y);
+		System.out.println(" Static initialliser " + y);
     	
     }
 }
