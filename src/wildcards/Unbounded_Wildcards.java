f603b93cbd9b683beefc7e984387a7c7f7b3401b
cchira <cchira.temenos.com>
2020-01-20 14:34:19 +0200

    wild card

--- src/wildcards/Unbounded_Wildcards.java
+++ src/wildcards/Unbounded_Wildcards.java
@@ -1,10 +1,21 @@
 package wildcards;
 
+import java.util.Collection;
+import java.util.ArrayList;
+
 public class Unbounded_Wildcards {
 
-	public static void main(String[] args) {
-		
-
+	static void printCollection(Collection<?> c) {
+		// a wildcard collection
+		for (Object o : c) {
+			System.out.println(o);
+		}
 	}
 
+	public static void main(String[] args) {
+		Collection<String> cs = new ArrayList<String>();
+		cs.add("hello");
+		cs.add("world");
+		printCollection(cs);
+	}
 }
