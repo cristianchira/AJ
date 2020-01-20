26d863565051b75cc8c89eac69e2604d0f781de8
cchira <cchira.temenos.com>
2020-01-20 17:36:18 +0200

    update

--- src/classes/Point.java
+++ src/classes/Point.java
@@ -8,6 +8,8 @@ public class Point {
 	int[] metrics;
     public Point(int x, int y) { this.x = x; this.y = y; }
     public static final Point origin = new Point(0, 0);
+    int[] w = new int[10]; // w[0] is an array component
+ 
     
 	public static void main(String[] args) {
 		
@@ -19,4 +21,21 @@ public class Point {
 		System.out.println(" Static initialliser " + y);
     	
     }
+    
+	public int[] populate(int n) {
+		int[] pop = new int[n];
+		for (int i = 0; i < n; i++) {
+			pop[i] = i;
+		}
+
+		return pop;
+	}
+
+	public int arraySum(int[] array) {
+		int sum = 0;
+		for (int i : array) {
+			sum += i;
+		}
+		return sum;
+	}
 }
