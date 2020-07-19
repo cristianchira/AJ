c74f35038b6e49d81c38f87ebe629b1f338b1ab5
cristianchira <cristianchira@gmail.com>
2020-07-19 09:29:02 +0300

    updateLocalClassExample

--- src/classes/LocalClass/LocalClassExample.java
+++ src/classes/LocalClass/LocalClassExample.java
@@ -2,59 +2,55 @@ package classes.LocalClass;
 
 public class LocalClassExample {
 
-	static String regularExpression = "[^0-9]";
+    static String regularExpression = "[^0-9]";
 
-	public static void validatePhoneNumber(String phoneNumber1, String phoneNumber2) {
+    public static void validatePhoneNumber(String phoneNumber1, String phoneNumber2) {
 
-		final int numberLength = 10;
+        final int numberLength = 10;
 
-		// Valid in JDK 8 and later:
+        class PhoneNumber {
 
-		// int numberLength = 10;
+            String formattedPhoneNumber = null;
 
-		class PhoneNumber {
+            PhoneNumber(String phoneNumber) {
+                // numberLength = 7;
+                String currentNumber = phoneNumber.replaceAll(regularExpression, "");
+                if (currentNumber.length() == numberLength)
+                    formattedPhoneNumber = currentNumber;
+                else
+                    formattedPhoneNumber = null;
+            }
 
-			String formattedPhoneNumber = null;
+            public String getNumber() {
+                return formattedPhoneNumber;
+            }
 
-			PhoneNumber(String phoneNumber) {
-				// numberLength = 7;
-				String currentNumber = phoneNumber.replaceAll(regularExpression, "");
-				if (currentNumber.length() == numberLength)
-					formattedPhoneNumber = currentNumber;
-				else
-					formattedPhoneNumber = null;
-			}
+            // Valid in JDK 8 and later:
 
-			public String getNumber() {
-				return formattedPhoneNumber;
-			}
+            public void printOriginalNumbers() {
+                System.out.println("Original numbers are " + phoneNumber1 + " and " + phoneNumber2);
+            }
+        }
 
-			// Valid in JDK 8 and later:
+        PhoneNumber myNumber1 = new PhoneNumber(phoneNumber1);
+        PhoneNumber myNumber2 = new PhoneNumber(phoneNumber2);
 
-			public void printOriginalNumbers() {
-				System.out.println("Original numbers are " + phoneNumber1 + " and " + phoneNumber2);
-			}
-		}
+        // Valid in JDK 8 and later:
 
-		PhoneNumber myNumber1 = new PhoneNumber(phoneNumber1);
-		PhoneNumber myNumber2 = new PhoneNumber(phoneNumber2);
+        myNumber1.printOriginalNumbers();
 
-		// Valid in JDK 8 and later:
+        if (myNumber1.getNumber() == null)
+            System.out.println("First number is invalid");
+        else
+            System.out.println("First number is " + myNumber1.getNumber());
+        if (myNumber2.getNumber() == null)
+            System.out.println("Second number is invalid");
+        else
+            System.out.println("Second number is " + myNumber2.getNumber());
 
-		myNumber1.printOriginalNumbers();
+    }
 
-		if (myNumber1.getNumber() == null)
-			System.out.println("First number is invalid");
-		else
-			System.out.println("First number is " + myNumber1.getNumber());
-		if (myNumber2.getNumber() == null)
-			System.out.println("Second number is invalid");
-		else
-			System.out.println("Second number is " + myNumber2.getNumber());
-
-	}
-
-	public static void main(String... args) {
-		validatePhoneNumber("123-456-7890", "456-7890");
-	}
+    public static void main(String... args) {
+        validatePhoneNumber("123-456-7890", "456-7890");
+    }
 }
