e73386fbafd8db8276181b330a0286d9d506fa7b
cristianchira <cristianchira@gmail.com>
2020-07-19 08:59:40 +0300

    updatemain

--- src/classes/LocalClass/LocalClassMain.java
+++ src/classes/LocalClass/LocalClassMain.java
@@ -16,11 +16,30 @@ public class LocalClassMain {
 
             @Override
             public void onClick(String title) {
-
+                System.out.println(title + " was clicked");
             }
         }
         btnPrint.setOnClickListener(new ClickListener());
+        listen();
     }
 
+    public static void listen() {
+        boolean quit = false;
+        while (!quit) {
+            System.out.println("Enter your choice: ");
+            int ins = scanner.nextInt();
+            scanner.nextLine();
+            switch (ins) {
+                case 0:
+                    quit = true;
+                    System.out.println("Exit...");
+                    break;
+                case 1:
+                    btnPrint.onClick();
+                    break;
+
+            }
+        }
+    }
 
 }
