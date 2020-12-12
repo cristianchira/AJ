934902bdb93e050569ef9a496f61c67de8e09784
cristianchira <cristianchira@gmail.com>
2020-12-12 18:13:17 +0200

    add all

---
+++ src/maps/tim/set_and_hashset/HeavenlyBody.java
@@ -0,0 +1,37 @@
+package maps.tim.set_and_hashset;
+
+import java.util.HashSet;
+import java.util.Set;
+/*
+* A Set is a Collection that cannot contain duplicate elements
+* The Java platform contains three general-purpose Set implementations: HashSet, TreeSet, and LinkedHashSet
+*
+* */
+
+public final class HeavenlyBody {
+    private final String name;
+    private final double orbitalPeriod;
+    private final Set<HeavenlyBody> satellites;
+
+    public HeavenlyBody(String name, double orbitalPeriod) {
+        this.name = name;
+        this.orbitalPeriod = orbitalPeriod;
+        this.satellites = new HashSet<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getOrbitalPeriod() {
+        return orbitalPeriod;
+    }
+
+    public boolean addMoon(HeavenlyBody moon) {
+        return this.satellites.add(moon);
+    }
+
+    public Set<HeavenlyBody> getSatellites() {
+        return new HashSet<>(this.satellites);
+    }
+}
