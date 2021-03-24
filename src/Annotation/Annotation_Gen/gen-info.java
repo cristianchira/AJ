38c41fcb78e38437fdb1dc36221ef4a602f39d63
cristianchira <cristianchira@gmail.com>
2021-03-24 21:21:28 +0200

    update

---
+++ src/Annotation/Annotation_Gen/gen-info.java
@@ -0,0 +1,35 @@
+package Annotation.Annotation_Gen;
+/*
+https://docs.oracle.com/javase/8/docs/api/index.html
+
+     java.lang.annotation
+
+
+        Interfaces
+
+            Annotation
+
+        Enums
+
+            ElementType
+            RetentionPolicy
+
+        Exceptions
+
+            AnnotationTypeMismatchException
+            IncompleteAnnotationException
+
+        Errors
+
+            AnnotationFormatError
+
+        Annotation Types
+
+            Documented
+            Inherited
+            Native
+            Repeatable
+            Retention
+            Target
+
+  */
