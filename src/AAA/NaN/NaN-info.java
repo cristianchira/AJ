9496cf6395c20bb2e000172474458372919d7d9a
cristianchira <cristianchira@gmail.com>
2021-05-06 07:32:28 +0300

    q

---
+++ src/AAA/NaN/NaN-info.java
@@ -0,0 +1,12 @@
+package AAA.NaN;
+
+// https://www.baeldung.com/java-not-a-number
+
+/*                              NaN
+
+    is a numeric data type value which stands for “not a number”.
+
+
+
+
+ */
