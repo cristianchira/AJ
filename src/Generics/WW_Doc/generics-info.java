197c3b9310ae5c24b473e8dcec3389369f2ab510
cristianchira <cristianchira@gmail.com>
2021-05-10 09:48:42 +0300

    update

---
+++ src/Generics/WW_Doc/generics-info.java
@@ -0,0 +1,9 @@
+package Generics.WW_Doc;
+
+/*                              Generics
+
+    https://docs.oracle.com/javase/tutorial/java/generics/upperBounded.html
+
+
+
+ */
---
+++ src/Interfaces/WW_Doc/interfaces-info.java
@@ -0,0 +1,33 @@
+package Interfaces.WW_Doc;
+
+/*                                                 Interfaces
+
+    https://docs.oracle.com/javase/specs/jls/se8/html/jls-9.html
+
+
+    An interface declaration introduces a new reference type whose members are classes, interfaces, constants, and methods.
+        This type has no instance variables, and typically declares one or more abstract methods; otherwise unrelated
+        classes can implement the interface by providing implementations for its abstract methods. Interfaces may not
+        be directly instantiated.
+
+        • A nested interface is any interface whose declaration occurs within the body of another class or interface.
+
+        • A top level interface is an interface that is not a nested interface.
+
+    We distinguish between two kinds of interfaces:
+
+        • normal interfaces
+        • and annotation types
+
+                                           Difference between interfaces and abstract classes
+
+   https://www.javatpoint.com/difference-between-abstract-class-and-interface
+
+                                                 Functional interfaces
+
+    https://docs.oracle.com/javase/8/docs/api/java/util/function/package-summary.html
+
+
+
+
+ */
