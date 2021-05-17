788cb4c7fb93121b582b40a695e11882e5653be1
cristianchira <cristianchira@gmail.com>
2021-05-17 08:03:44 +0300

    update

--- src/util/interfaces/Collection/Collection_Example/Void_Clear/Void_Clear_Class.java
+++ src/util/interfaces/Collection/Collection_Example/Void_Clear/Void_Clear_Class.java
@@ -3,7 +3,17 @@ package util.interfaces.Collection.Collection_Example.Void_Clear;
 import java.util.Collection;
 import java.util.concurrent.LinkedBlockingDeque;
 
-public class Clear_Class {
+/**
+ * Removes all of the elements from this collection (optional operation).
+ * The collection will be empty after this method returns.
+ *
+ * @throws UnsupportedOperationException if the <tt>clear</tt> operation
+ *         is not supported by this collection
+ */
+
+//    void clear();
+
+public class Void_Clear_Class {
     public static void main(String[] args) {
         Collection<Integer> collection = new LinkedBlockingDeque<Integer>();
         collection.add(23);
